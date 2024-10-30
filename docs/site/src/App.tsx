627fb9687172b735d474118ba11e4641a1eaeebc
Decker Ayers <69859630+ayersdecker@users.noreply.github.com>
2024-10-30 03:33:22 +0000

    Nav start

--- docs/site/src/App.tsx
+++ docs/site/src/App.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, Stack } from 'react-bootstrap';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Decker</p>
+        <Stack>
+          <Stack direction="horizontal" className='centered' gap={2}>
+            <Button variant="primary">Home</Button>
+            <Button variant="primary">About</Button>
+            <Button variant="primary">Contact</Button>
+          </Stack>
+        </Stack>
       </header>
+      <body className='App-body'>
+      </body>
     </div>
   );
 }
